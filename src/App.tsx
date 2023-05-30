63f524b31c9dffabaae87df248a6157ca5d7c041
daryna-zinchenko <zinchenko.daryna@gmail.com>
2023-05-30 05:15:00 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Hello, TypeScript and SCSS!</h1>
+    </div>
+  );
+}
+
+export default App;
