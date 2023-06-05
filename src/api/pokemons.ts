12e0fc50d32be49495f4ce3bad0866c895a98b40
daryna-zinchenko <zinchenko.daryna@gmail.com>
2023-06-05 17:12:48 +0200

    added error handling

--- src/api/pokemons.ts
+++ src/api/pokemons.ts
@@ -1,22 +1,31 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { Pokemon } from '../types/pokemon';
 import { PokemonsData, Snippet } from '../types/pokemonsData';
 
 export function getLinks(url: string): Promise<PokemonsData> {
   return axios.get(url)
-    .then(response => response.data);
+    .then((response: AxiosResponse<PokemonsData>) => response.data)
+    .catch((error: any) => {
+      throw new Error(`Error occurred while fetching data: ${error}`);
+    });
 }
 
 export const getAll = (url: string) => {
   return getLinks(url)
-    .then(data => {
+    .then((data: PokemonsData) => {
       const nextUrl = data.next;
       const pokemons = Promise.all(
         data.results.map((snippet: Snippet) => {
           return axios.get<Pokemon>(snippet.url)
-            .then(response => response.data);
+            .then((response: AxiosResponse<Pokemon>) => response.data)
+            .catch((error: any) => {
+              throw new Error(`Error occurred while fetching data: ${error}`);
+            });
         })
       );
-      return {pokemons, nextUrl};
+      return { pokemons, nextUrl };
+    })
+    .catch((error: any) => {
+      throw new Error(`Error occurred while fetching data: ${error}`);
     });
 };
